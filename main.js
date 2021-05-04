117ebcf8e75d58930d8b311d0c226fe94c1aa39b
Aryankishor <73214720+Aryankishor@users.noreply.github.com>
2021-05-04 20:22:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("My first console project");
+var first_name="Aryan";
+var last_name="Kishor";
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+var a=10;
+var b=5;
+var add=a+b;
+console.log(add);
+var minus=a-b;
+console.log(minus);
+var divide=a/b;
+console.log(divide);
+var multiply=a*b;
+console.log(multiply);
